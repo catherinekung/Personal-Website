218134b83dc6919ed1dddd2b84592decca5eac14
Catherine Kung <catherinekung14@gmail.com>
2021-11-25 13:29:00 -0800

    revised info and added animation

--- index.js
+++ index.js
@@ -1,101 +1,77 @@
-window.onscroll = function() {
-  alterColor()
-};
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
-var page1 = 1;
-var page2 = 0;
-var page3 = 0;
-var page4 = 0;
-
-function alterColor() {
-  if (document.body.scrollTop > 728 && document.body.scrollTop < 1500 || document.documentElement.scrollTop > 728 && document.documentElement.scrollTop < 1500){
-    document.getElementById("navbar").classList = "";
-    document.getElementById("navbar").classList.add("page2");
-    document.getElementById("aboutPage").style.color = "rgb(73, 83, 94)";
-    document.getElementById("homePage").style.color = "white";
-    document.getElementById("experiencePage").style.color = "white";
-    page2 = 1;
-    page1 = 0;
-    page3 = 0;
-    page4 = 0;
+function scrollNavigator() {
+  const pages = {
+    "home": "main",
+    "aboutme": "gradientDark",
+    "experience": "gradientMedium",
+    "projects": "gradientLight"
   }
-  else {
-    if (document.body.scrollTop > 1500 && document.body.scrollTop < 2170 || document.documentElement.scrollTop > 1500 && document.documentElement.scrollTop < 2170){ 
-      document.getElementById("navbar").classList = "";
-      document.getElementById("navbar").classList.add("page3");
-      document.getElementById("aboutPage").style.color = "white";
-      document.getElementById("experiencePage").style.color = "rgb(73, 83, 94)";
-      document.getElementById("projectsPage").style.color = "white";
-      page3 = 1;
-      page1 = 0;
-      page2 = 0;
-      page4 = 0;
-    }
-    else {
-      if (document.body.scrollTop > 2170 || document.documentElement.scrollTop > 2170) {
-        document.getElementById("navbar").classList = ""; 
-        document.getElementById("navbar").classList.add("page4");
-        document.getElementById("projectsPage").style.color = "rgb(73, 83, 94)";
-        document.getElementById("experiencePage").style.color = "white";
-        document.getElementById("homePage").style.color = "white";
-        page4 = 1;
-        page1 = 0;
-        page3 = 0;
-        page2 = 0;
+  let scrollY = window.pageYOffset;
+  document.querySelectorAll("section").forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 75;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector("a[href*=" + sectionId + "]").classList.add("active");
+      document.getElementById("navbar").classList = ""
+      document.getElementById("navbar").classList.add(pages[sectionId]);
+      if (sectionId == "aboutme") {
+        document.querySelector(".bio").classList.add("bounceZoom");
       }
-      else{
-        document.getElementById("navbar").classList = "";
-        document.getElementById("aboutPage").style.color = "white";
-        document.getElementById("homePage").style.color = "rgb(73, 83, 94)";
-        document.getElementById("projectsPage").style.color = "white";
-        page1 = 1;
-        page2 = 0;
-        page3 = 0;
-        page4 = 0;
+    } else {
+      document.querySelector("a[href*=" + sectionId + "]").classList.remove("active");
+      if (sectionId == "home") {
+        document.getElementById("navbar").classList.add(pages[sectionId]);
       }
     }
-  }
+  });
+
+  document.querySelectorAll(".experiencePosition").forEach(current => {
+    if (isElementInViewport(current)) {
+      current.classList.add("bounceRight");
+    }
+  });
+
+  document.querySelectorAll(".timelineEntry").forEach(current => {
+    if (isElementInViewport(current)) {
+      current.classList.add("bounceRight");
+    }
+  });
 }
 
-function hover(s){
-    document.getElementById(s).style.color = "rgb(73, 83, 94)";
-}
+window.addEventListener("scroll", scrollNavigator);
+window.addEventListener("load", scrollNavigator)
 
-function unhover(s){
-  if (s == 'homePage' && page1 == 1 || s == 'aboutPage' && page2 == 1 || s == "experiencePage" && page3 == 1 || s == "projectsPage" && page4 == 1)
-    document.getElementById(s).style.color = "rgb(73, 83, 94)";
-  else
-    document.getElementById(s).style.color = "white";
-}
+function showSlide(n) {
+  document.querySelectorAll(".slide").forEach(current => {
+    const slide = "slide" + n;
+    const currentSlide = current.getAttribute("id");
 
+    if (currentSlide == slide) {
+      current.classList.remove("notActive");
+    } else {
+      current.classList.add("notActive");
+    }
+  });
 
+  document.querySelectorAll(".dot").forEach(current => {
+    const dot = "project" + n;
+    const currentDot = current.getAttribute("id");
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-
-function advanceSlide(n) {
-  showSlides(slideIndex += n);
-}
-
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-      dots[i].className = dots[i].className.replace(" dot1", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    if (currentDot == dot) {
+      current.classList.add("activeDot");
+    } else {
+      current.classList.remove("activeDot");
+    }
+  });
 }
